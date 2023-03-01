e1b2bb647ea1753aeebb9ad23eb0fbcc8830d188
JoshuaJones27 <100417295+JoshuaJones27@users.noreply.github.com>
2023-03-01 10:14:14 +0000

    Onde andas?

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from './Components/Button/Button';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return ( 
+    <>
+    Im here !
+    <Button title={'Test'} disable={false} type={'add'}/>
+    <Button title={'Test'} disable={false} type={'remove'}/>
+    <Button title={'Test'} disable={false} type={'checkout'}/>
+    </>
   );
+  
 }
 
 export default App;
