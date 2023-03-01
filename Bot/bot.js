9714f198b7ca3d2205fcdb38a978d4d6dca1178e
JoshuaJones27 <100417295+JoshuaJones27@users.noreply.github.com>
2023-03-01 17:43:34 +0000

    Feito

---
+++ Bot/bot.js
@@ -0,0 +1,15 @@
+const { Telegraf } = require("telegraf");
+const TOKEN = "6247160381:AAGahUyItYDNJYwhotQGV672zNddIJ9ifPA";
+const bot = new Telegraf(TOKEN);
+
+const web_link = "https://superlative-fudge-74a999.netlify.app/";
+
+bot.start((ctx) =>
+  ctx.reply("Welcome :)))))", {
+    reply_markup: {
+      keyboard: [[{ text: "web app", web_app: { url: web_link } }]],
+    },
+  })
+);
+
+bot.launch();
