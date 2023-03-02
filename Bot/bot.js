ebebbd356c7380ded25eb8393a5faa15e9097b16
JoshuaJones27 <100417295+JoshuaJones27@users.noreply.github.com>
2023-03-02 11:17:59 +0000

    receba

--- Bot/bot.js
+++ Bot/bot.js
@@ -1,5 +1,5 @@
 const { Telegraf } = require("telegraf");
-const TOKEN = "6247160381:AAGahUyItYDNJYwhotQGV672zNddIJ9ifPA";
+const TOKEN = "6247160381:AAG6-UtpUhuvMMfM-7ssb9FLualoMKQ1KT8";
 const bot = new Telegraf(TOKEN);
 
 const web_link = "https://warm-hotteok-d3c887.netlify.app/";
