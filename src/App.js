d28b3265d49e947b6593772b7cd9472e04322bbc
JoshuaJones27 <100417295+JoshuaJones27@users.noreply.github.com>
2023-03-02 12:39:58 +0000

    prettier

--- src/App.js
+++ src/App.js
@@ -1,65 +1,65 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import Card from './Components/Card/Card';
-import Cart from './Components/Cart/Cart';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Card from "./Components/Card/Card";
+import Cart from "./Components/Cart/Cart";
 
-const { getData } = require('./db/db');
+const { getData } = require("./db/db");
 
 const foods = getData();
 
-const tele = window.Telegram.WebApp
+const tele = window.Telegram.WebApp;
 
 function App() {
-
   const [cartItems, setCartItems] = useState([]);
 
   useEffect(() => {
     tele.ready();
-  })
+  });
 
   const onAdd = (food) => {
     const exist = cartItems.find((x) => x.id === food.id);
-    if(exist) {
-      setCartItems(cartItems.map((x) => 
-        x.id === food.id ? {...exist, quantity: exist.quantity + 1} : x
-      )
-     );
+    if (exist) {
+      setCartItems(
+        cartItems.map((x) =>
+          x.id === food.id ? { ...exist, quantity: exist.quantity + 1 } : x
+        )
+      );
+    } else {
+      setCartItems([...cartItems, { ...food, quantity: 1 }]);
     }
-    else {
-      setCartItems([...cartItems, {...food, quantity: 1}]);
-    }
-  }
+  };
 
   const onRemove = (food) => {
     const exist = cartItems.find((x) => x.id === food.id);
-    if(exist.quantity === 1) {
+    if (exist.quantity === 1) {
       setCartItems(cartItems.filter((x) => x.id !== food.id));
+    } else {
+      setCartItems(
+        cartItems.map((x) =>
+          x.id === food.id ? { ...exist, quantity: exist.quantity - 1 } : x
+        )
+      );
     }
-    else {
-      setCartItems(cartItems.map((x) => 
-        x.id === food.id ? {...exist, quantity: exist.quantity - 1} : x
-      )
-     );
-    }
-  }
+  };
 
   const onCheckout = () => {
-    tele.MainButton.text = "Pay"
-    tele.MainButton.show()
-  }
+    tele.MainButton.text = "Pay";
+    tele.MainButton.show();
+  };
 
-  return ( 
+  return (
     <>
       <h1 className="heading">Food Order</h1>
-      <Cart cartItems={cartItems} onCheckout={onCheckout}/>
+      <Cart cartItems={cartItems} onCheckout={onCheckout} />
       <div className="cards__container">
         {foods.map((food) => {
-          return <Card food = {food} key={food.id} onAdd={onAdd} onRemove={onRemove}/>
+          return (
+            <Card food={food} key={food.id} onAdd={onAdd} onRemove={onRemove} />
+          );
         })}
       </div>
     </>
   );
-  
 }
 
 export default App;
